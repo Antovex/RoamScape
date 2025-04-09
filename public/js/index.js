d1765a3190a8c4b3f914f49b9b910793ec7c55e3
Antarin Ghosal <antaringhosal@gmail.com>
2025-04-09 22:38:46 +0530

    added bookingController to handle checkout session creation with Stripe.

--- public/js/index.js
+++ public/js/index.js
@@ -1,21 +1,24 @@
-/* eslint-disable */ 
+/* eslint-disable */
 import '@babel/polyfill';
 import { displayMap } from './mapbox';
 import { login, logout } from './login';
 import { updateSettings } from './updateSettings';
+import { bookTour } from './stripe';
 
 const mapbox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
 const logoutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
 const userPasswordForm = document.querySelector('.form-user-password');
+// const bookBtn = document.getElementById('book-tour');
+const bookBtns = document.querySelectorAll('.book-tour-btn');
 
-if(mapbox) {
+if (mapbox) {
     const locations = JSON.parse(mapbox.dataset.locations);
     displayMap(locations);
 }
 
-if(loginForm) {
+if (loginForm) {
     loginForm.addEventListener('submit', (e) => {
         e.preventDefault();
         const email = document.getElementById('email').value;
@@ -24,11 +27,11 @@ if(loginForm) {
     });
 }
 
-if(logoutBtn) {
+if (logoutBtn) {
     logoutBtn.addEventListener('click', logout);
 }
 
-if(userDataForm) {
+if (userDataForm) {
     userDataForm.addEventListener('submit', (e) => {
         e.preventDefault();
 
@@ -42,20 +45,44 @@ if(userDataForm) {
     });
 }
 
-if(userPasswordForm) {
+if (userPasswordForm) {
     userPasswordForm.addEventListener('submit', async (e) => {
         e.preventDefault();
 
         // document.querySelector('.btn--save-password').innerHTML = 'Updating...';
 
-        const passwordCurrent = document.getElementById('password-current').value;
+        const passwordCurrent =
+            document.getElementById('password-current').value;
         const password = document.getElementById('password').value;
-        const passwordConfirm = document.getElementById('password-confirm').value;
-        await updateSettings({passwordCurrent, password, passwordConfirm}, 'password');
+        const passwordConfirm =
+            document.getElementById('password-confirm').value;
+        await updateSettings(
+            { passwordCurrent, password, passwordConfirm },
+            'password',
+        );
 
         // document.querySelector('.btn--save-password').innerHTML = 'Save password';
         document.getElementById('password-current').value = '';
         document.getElementById('password').value = '';
         document.getElementById('password-confirm').value = '';
+    });
+}
+
+// console.log(bookBtn);
+
+// if (bookBtn)
+//     bookBtn.addEventListener('click', (e) => {
+//         // e.target.textContent = 'Processing...';
+//         const { tourId } = e.target.dataset;
+//         bookTour(tourId);
+//     });
+
+if (bookBtns) {
+    bookBtns.forEach((btn) => {
+        btn.addEventListener('click', (e) => {
+            e.target.textContent = 'Processing...';
+            const { tourId } = e.target.dataset;
+            bookTour(tourId);
+        });
     });
 }
---
+++ public/js/stripe.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import { showAlert } from './alerts';
+
+export const bookTour = async tourId => {
+    const stripe = Stripe('pk_test_51RBBRS4g4ndWAMg5bM5v0lBB7WECbSs1A7zGwkEumauKiUePEHe7lLmRFXjWsf9xC1KJetSpbfSSn10ggrgI5wrD00csvAw74G');
+    try {
+        // 1) Get checkout session from API
+        const session = await axios(
+            `http://localhost:3000/api/v1/bookings/checkout-session/${tourId}`
+        );
+        
+        // 2) Create checkout form + charge credit card
+        await stripe.redirectToCheckout({
+            sessionId: session.data.session.id
+        });
+    } catch (err) {
+        console.log(err);
+        showAlert('error', err);
+    }
+};
