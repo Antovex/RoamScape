72f82f750cb686bd7a40472f296a52e45cb12dc3
Antarin Ghosal <antaringhosal@gmail.com>
2025-04-19 15:00:23 +0530

    bug fix

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
+
+const app = express();
+app.set('trust proxy', 1);
+
+
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
@@ -17,7 +22,6 @@ const reviewRouter = require('./routes/reviewRoutes');
 const viewRouter = require('./routes/viewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
 
-const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
